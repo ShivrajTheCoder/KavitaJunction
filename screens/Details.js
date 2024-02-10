344d0d061f04e23f56c166521037852df50a84fa
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-02-10 23:32:00 +0530

    scroll-issue-fixed

--- screens/Details.js
+++ screens/Details.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import UserInfo from '../components/DetailsComponents/UserInfo'
-import CategoriesSlider from '../components/DetailsComponents/CategoriesSlider'
-import ProfileContainer from '../components/DetailsComponents/ProfileContainer'
-import QuickPlayContainer from '../components/DetailsComponents/QuickPlayContainer'
+import React from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import UserInfo from '../components/DetailsComponents/UserInfo';
+import CategoriesSlider from '../components/DetailsComponents/CategoriesSlider';
+import ProfileContainer from '../components/DetailsComponents/ProfileContainer';
+import QuickPlayContainer from '../components/DetailsComponents/QuickPlayContainer';
 
 export default function Details() {
   return (
-    <View style={styles.container}>
-        <UserInfo/>
-        <CategoriesSlider/>
-        <ProfileContainer/>
-        <QuickPlayContainer/>
-    </View>
-  )
+    <ScrollView contentContainerStyle={styles.scrollView}>
+      <View style={styles.container}>
+        <UserInfo />
+        <CategoriesSlider />
+        <ProfileContainer />
+        <QuickPlayContainer />
+      </View>
+    </ScrollView>
+  );
 }
-const styles=StyleSheet.create({
-  container:{
-    backgroundColor:"#022b3a",
-    // height:"100%",
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+    backgroundColor: "#022b3a",
+  },
+  container: {
+    paddingBottom: 20, 
   }
-})
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import Banner from '../components/HomeComponents/Banner'
-import BhajanContainer from '../components/HomeComponents/BhajanContainer'
-import SongContainer from '../components/HomeComponents/SongsContainer'
+import React from 'react';
+import { StyleSheet, ScrollView, View } from 'react-native';
+import Banner from '../components/HomeComponents/Banner';
+import BhajanContainer from '../components/HomeComponents/BhajanContainer';
+import SongContainer from '../components/HomeComponents/SongsContainer';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-        <Banner/>
-        <BhajanContainer/>
-        <SongContainer/>
-    </View>
-  )
+    <ScrollView contentContainerStyle={styles.scrollView}>
+      <View style={styles.container}>
+        <Banner />
+        <BhajanContainer />
+        <SongContainer />
+      </View>
+    </ScrollView>
+  );
 }
 
-const styles=StyleSheet.create({
-  container:{
-    backgroundColor:"#022b3a",
+const styles = StyleSheet.create({
+  scrollView: {
+    flexGrow: 1,
+    backgroundColor: "#022b3a",
+  },
+  container: {
+    paddingBottom: 20, 
   }
-})
+});
