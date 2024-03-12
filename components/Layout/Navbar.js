c58dd842a2042a7f2dd24e09b1e37ac040bcff5c
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-03-12 16:18:07 +0530

    popup-added

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, StyleSheet, Dimensions, Text, Modal, Button } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, Dimensions, Text, Modal, Button, ScrollView } from 'react-native';
 import { SimpleLineIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
 import { EvilIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+const closeIcon = <AntDesign name="close" size={24} color="black" />;
 
 const Navbar = ({ state, descriptors, navigation }) => {
   const iconRoutes = {
@@ -57,8 +59,44 @@ const Navbar = ({ state, descriptors, navigation }) => {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <Text>Subscription Popup Content</Text>
-            <Button title="Close" onPress={toggleModal} />
+            <View style={styles.headingCont}>
+              {/* <TouchableOpacity style={styles.modalCloseButton} onPress={() => setModalVisible(!modalVisible)}>
+                {closeIcon}
+              </TouchableOpacity> */}
+              <Text style={styles.modalHeading}>You have a growth mindset</Text>
+            </View>
+            {/* <Text style={styles.modalText}>You have unlocked a coupon</Text> */}
+            <Text style={styles.modalText}>VALID ONLY FOR TODAY</Text>
+
+            <TouchableOpacity style={styles.whatsappButton}>
+              <Text style={styles.whatsappButtonText}>Whatsapp us within 7 days for a 100% refund</Text>
+            </TouchableOpacity>
+            <View style={{ flexDirection: "row", justifyContent: "space-between" }} >
+              <View style={{ flexDirection: "column" , alignItems:"center", justifyContent:"center", gap:5, padding:10,borderWidth:1,borderRadius:10,borderColor:"white" ,marginVertical:10}} >
+                <View>
+                  <Text style={{color:"white", textDecorationLine:"line-through" }} >₹ 100.00</Text>
+                  <Text style={{color:"white"}}>₹ 200.00</Text>
+                </View>
+                <Text style={{color:"white"}}>12 months</Text>
+              </View>
+              <View style={{ flexDirection: "column" , alignItems:"center", justifyContent:"center", gap:5, padding:10,borderWidth:1,borderRadius:10,borderColor:"white" ,marginVertical:10,marginHorizontal:10}} >
+                <View>
+                  <Text style={{color:"white", textDecorationLine:"line-through" }} >₹ 100.00</Text>
+                  <Text style={{color:"white"}}>₹ 200.00</Text>
+                </View>
+                <Text style={{color:"white"}}>12 months</Text>
+              </View>
+              <View style={{ flexDirection: "column" , alignItems:"center", justifyContent:"center", gap:5, padding:10,borderWidth:1,borderRadius:10,borderColor:"white" ,marginVertical:10}} >
+                <View>
+                  <Text style={{color:"white", textDecorationLine:"line-through" }} >₹ 100.00</Text>
+                  <Text style={{color:"white"}}>₹ 200.00</Text>
+                </View>
+                <Text style={{color:"white"}}>12 months</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.paymentButton}>
+              <Text style={styles.paymentButtonText}>Payment Options</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </Modal>
@@ -103,11 +141,56 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalContent: {
-    backgroundColor: '#fff',
-    padding: 20,
+    backgroundColor: '#000', // Black background color
+    // padding: 20,
     borderRadius: 10,
     alignItems: 'center',
   },
+  modalHeading: {
+    color: 'black', // Golden text color
+    marginBottom: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalCloseButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  modalText: {
+    color: '#fca311', // Golden text color
+    // marginBottom: 10,
+    marginVertical: 40
+  },
+  whatsappButton: {
+    backgroundColor: '#fca311', // Golden background color
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  whatsappButtonText: {
+    color: '#000', // Black text color
+    fontWeight: 'bold',
+  },
+  paymentButton: {
+    backgroundColor: '#fca311', // Golden background color
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 20,
+    paddingHorizontal:20
+  },
+  paymentButtonText: {
+    color: '#000', // Black text color
+    fontWeight: 'bold',
+  },
+  headingCont: {
+    width: 385,
+    backgroundColor: "#fca311",
+    paddingVertical: 15,
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10
+  }
 });
 
 export default Navbar;
