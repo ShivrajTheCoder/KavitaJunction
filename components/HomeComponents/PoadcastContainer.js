2e6e6ee494d6d8447d65f83037d68bda1d05fdca
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-03-27 13:19:15 +0530

    podcast-category-api-integrated

--- components/HomeComponents/PoadcastContainer.js
+++ components/HomeComponents/PoadcastContainer.js
@@ -1,30 +1,68 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import ThemeContext from '../../contexts/ThemeProvider';
+import axios from 'axios';
 
 export default function PodcastContainer() {
-  // Sample podcast data
-  const podcasts = [
-    { category: 'Cinema', backgroundColor: '#ff6347' },
-    { category: 'Sangeet', backgroundColor: '#4682b4' },
-    { category: 'Sahitya', backgroundColor: '#2e8b57' },
-    { category: 'Shiksha', backgroundColor: '#f4a460' },
-    { category: 'Social', backgroundColor: '#9370db' },
-    { category: 'Popular', backgroundColor: '#ffa500' },
-  ];
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setError(null);
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/category/getallcategories`);
+        if (response.status === 200) {
+          const { categories } = response.data;
+          setCategories(categories);
+        } else {
+          console.error('Failed to fetch categories:', response.statusText);
+          setError('Failed to fetch categories');
+        }
+      } catch (error) {
+        console.error('Error fetching categories:', error);
+        setError('Error fetching categories');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
 
   const { theme } = useContext(ThemeContext); // Access theme from ThemeContext
+  // const podcasts = [
+  //   { category: 'Cinema', backgroundColor: '#ff6347' },
+  //   { category: 'Sangeet', backgroundColor: '#4682b4' },
+  //   { category: 'Sahitya', backgroundColor: '#2e8b57' },
+  //   { category: 'Shiksha', backgroundColor: '#f4a460' },
+  //   { category: 'Social', backgroundColor: '#9370db' },
+  //   { category: 'Popular', backgroundColor: '#ffa500' },
+  // ];
+  // Function to generate random background color
+  const generateRandomColor = () => {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+  };
 
   return (
     <View style={styles.container}>
       <Text style={[styles.heading, { color: theme === 'dark' ? 'white' : 'black' }]}>Podcasts</Text>
-      <ScrollView horizontal contentContainerStyle={styles.scrollContainer} showsHorizontalScrollIndicator={false} >
-        {podcasts.map((podcast, index) => (
-          <View key={index} style={[styles.block, { backgroundColor: podcast.backgroundColor }]}>
-            <Text style={styles.text}>{podcast.category}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : error ? (
+        <Text>Error: {error}</Text>
+      ) : (
+        <ScrollView horizontal contentContainerStyle={styles.scrollContainer} showsHorizontalScrollIndicator={false}>
+          {categories.map((category, index) => (
+            <View key={index} style={[styles.block, { backgroundColor: generateRandomColor() }]}>
+              <Text style={styles.text}>{category.category_name}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 }
